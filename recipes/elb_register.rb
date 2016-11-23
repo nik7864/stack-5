2fc4ff7e1d446c1df906940592a473e4e22dee8b
nik7864 <sudipta1436@gmail.com>
2016-11-23 04:34:36 +0000

    Update elb_register.rb
    Removing region parameter from commands

--- recipes/elb_register.rb
+++ recipes/elb_register.rb
@@ -8,21 +8,21 @@
 #
 
 execute 'Regster to the CDS Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-cds-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region us-east-1"
+  command "aws elbv2 register-targets --target-group-arn #{node['public-cds-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id`"
 end
 
 execute 'Regster to the LAP Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-lap-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region us-east-1"
+  command "aws elbv2 register-targets --target-group-arn #{node['public-lap-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id`"
 end
 
 execute 'Regster to the LEC Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-lec-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region us-east-1"
+  command "aws elbv2 register-targets --target-group-arn #{node['public-lec-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id`"
 end
 
 execute 'Regster to the LED Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-led-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region us-east-1"
+  command "aws elbv2 register-targets --target-group-arn #{node['public-led-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id`"
 end
 
 execute 'Regster to the LMS Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-lms-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region us-east-1"
+  command "aws elbv2 register-targets --target-group-arn #{node['public-lms-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id`"
 end
