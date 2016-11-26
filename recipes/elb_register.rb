02c70e1d371d8c365219c148da52dd4dcfddf41d
nik7864 <sudipta1436@gmail.com>
2016-11-26 12:53:27 +0530

    Update elb_register
    Removed 4 lb registration

--- recipes/elb_register.rb
+++ recipes/elb_register.rb
@@ -22,19 +22,3 @@ end
 execute 'Regster to the LED Load Balancers' do
   command "aws elbv2 register-targets --target-group-arn #{node['public-led-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region #{node['current-region']}"
 end
-
-execute 'Regster to the LMS Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-lms-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region #{node['current-region']}"
-end
-
-execute 'Regster to the PBS Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-pbs-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region #{node['current-region']}"
-end
-
-execute 'Regster to the DCMS Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-dcms-demo']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region #{node['current-region']}"
-end
-
-execute 'Regster to the LMS2 Load Balancers' do
-  command "aws elbv2 register-targets --target-group-arn #{node['public-lms-demo-2']} --targets Id=`curl http://169.254.169.254/latest/meta-data/instance-id` --region #{node['current-region']}"
-end
